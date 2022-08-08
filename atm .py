45d862bf27b2525a168b261cc47eb95ab2178c31
Priti Kale <106419875+priti-kale@users.noreply.github.com>
2022-08-08 17:06:15 +0530

    Add files via upload

---
+++ atm .py
@@ -0,0 +1,84 @@
+#email= input("enter the email:")
+#if email>="1" and email<="9" and email>="A" and email<="a" and email>="Z" and email<="z" and email=="@" and email=="gmail.com ":
+#   print("click next")
+#   password=input("enter the password:")
+#   if (password=="number" or "alphabet" or "special character"):
+#           print("click ok")
+#           print("Thank you")
+#   else:
+#       print("enter valid password")
+#else:
+#    print("enter valid email id")
+#
+ #   print("enter the valid number")
+ 
+#byear=int(input("enter the birth  year"))
+#age=int(input("enter the year"))
+#if byear+age:
+ #     print(byear+age,"current year")
+
+
+#curentage=int(input("enter the curentage"))
+#birthage=int(input("enter the birthage"))
+#if curentage>birthage:
+#      print(curentage-birthage,"year")
+#else:
+ #     print("no age")
+
+#num=int(input("enter hte number"))
+#if num%2!=0 and num%3!=0 and num%5!=0 and num%7!=0 and num%9!=0 and num%11!=0:
+ #     print("prime number")
+#else:
+ #     print("not prime")
+         
+
+#print("atm program")
+#print("welcome to bannkworld swipe tour card")
+#print("english")
+#print("hindi")
+#amount=100000
+#lang=input("enter the language")
+#if lang=="english":
+#      pin=int(input("enter pin number"))
+#      if pin==9999:
+#            trans=input("enter transation")
+#            if trans=="withdrawal":
+#                  withdrawal=int(input("enter the withdrawal"))
+#                  if withdrawal<=amount:
+#                         print("you can take cash")
+#                  else:
+#                         print("please enter the valid amount")
+#            elif trans=="transfer money":
+#                  transfer_money =int(input("enter the transfer money"))
+#                  if transfer_money<amount:
+#                         print("your money has been transfered")
+#                  else:
+#                         print("please enter the valid amount")
+#            elif trans=="change your pin":
+#                  change_pin=int(input("enter the change pin "))
+#                  if change_pin>=1000:
+#                         print("your pin succesfully change")
+#                   else:       
+#                        print("please enter the valid pin")
+#            elif trans=="deposit":
+#                  deposit=int(input("enter the deposit amount"))
+#                  if deposit<=amount:
+#                         print("your depos has been succesful")
+#                  else:
+#                       print("please enter the valid amoun
+#      else:
+#         print("enter the valid pin")
+
+
+#basic_salary=input("enter your basic salary")
+#if basic_salary<=10000:
+#       da=basic_salary*(80/100)
+#      hra=basic_salary*(20/100)
+#      da=basic_salary*(90/100)
+#       hra=basic_salary*(25/100)
+#else:
+#       da=basic_salary*(95/100)
+#       hra=basic_salary*(30/100)
+#print("basic_salary+da+hra")
+       
+#
